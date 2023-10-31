a7982d7d376c4f44d43952dd7569e00e84e68b6c
devon <devoncowherd@gmail.com>
2023-10-31 09:37:01 -0700

    rebuild

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultNavComponent } from './default-nav/default-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultNavComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/default-nav/default-nav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-default-nav',
+  templateUrl: './default-nav.component.html',
+  styleUrls: ['./default-nav.component.css']
+})
+export class DefaultNavComponent {
+
+}
