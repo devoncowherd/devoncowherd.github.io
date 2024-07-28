8c25e57452ee8a69c0e8cb13a8f82a8821a09b57
Devon <71043147+devoncowherd@users.noreply.github.com>
2024-07-28 02:32:24 -0700

    Create main.js
    - Initial commit
    - Sets the view based upon user clicking tab

---
+++ assets/scripts/main.js
@@ -0,0 +1,23 @@
+//TODO: Cookie warning
+
+function showView(viewId) {
+      const views = document.querySelectorAll('.view');
+      views.forEach(view => view.classList.remove('active'));
+      document.getElementById(viewId).classList.add('active');
+
+document.querySelector(".HeaderText")
+    .textContent = viewId;
+    
+    }
+
+
+//TODO: create a hovering darkmode toggle. Follow browser by default
+function setDarkMode() {
+    
+}
+
+//TODO: 
+function coSo() {
+    
+}
+
