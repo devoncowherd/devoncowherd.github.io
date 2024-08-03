37cea68c70c741fe6f2a0ff0567baae43da34007
Devon <71043147+devoncowherd@users.noreply.github.com>
2024-08-03 05:30:39 -0700

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,23 +1,5 @@
-//TODO: Cookie warning
+const homepage = "devoncowherd.com";
+const delay = 2500;
+const redirect = window.location.assign(homepage);
 
-function showView(viewId) {
-      const views = document.querySelectorAll('.view');
-      views.forEach(view => view.classList.remove('active'));
-      document.getElementById(viewId).classList.add('active');
-
-document.querySelector(".HeaderText")
-    .textContent = viewId;
-    
-    }
-
-
-//TODO: create a hovering darkmode toggle. Follow browser by default
-function setDarkMode() {
-    
-}
-
-//TODO: 
-function coSo() {
-    
-}
-
+setTimeout(redirect, timeout);
